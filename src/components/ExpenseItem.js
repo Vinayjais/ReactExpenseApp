2c50621a04f388f12cd20b5545955a9414887a4b
Vinayjais <17vinayjaiswal>
2023-12-18 09:54:15 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+
+function ExpenseItem(){
+
+    return (
+     
+        <div className="Expense-item">
+                       
+                       <h3>Bike</h3>
+
+                       <div className="item">
+                       <div>Date</div>
+                      
+                         
+                     <div>Amount</div>
+                     </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
